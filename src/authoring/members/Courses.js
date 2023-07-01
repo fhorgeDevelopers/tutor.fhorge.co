3cfa487a300312baf3d836ba4ee1f00c9c2dbe3e
olaolumide <olajesujuwon@gmail.com>
2023-07-01 16:41:01 +0100

    4:35

--- src/authoring/members/Courses.js
+++ src/authoring/members/Courses.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { useNew } from '../../providers/New';
 import style from './courses.module.css';
-import { useLocation, useNavigate } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 
 const Courses = () => {
     const newHook = useNew();
